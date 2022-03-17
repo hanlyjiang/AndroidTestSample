f59c261e49661eaa1b478564bcaf6ac3eb66074b
hanlyjiang <hanlyjiang@outlook.com>
2022-03-17 22:59:40 +0800

    doc: update doc

--- app/src/main/java/com/github/hanlyjiang/sample/utils/StringUtils.java
+++ app/src/main/java/com/github/hanlyjiang/sample/utils/StringUtils.java
@@ -2,6 +2,9 @@ package com.github.hanlyjiang.sample.utils;
 
 public class StringUtils {
 
+    private StringUtils() {
+    }
+
     public static boolean isEmpty(String str) {
         return str == null || str.isEmpty();
     }
